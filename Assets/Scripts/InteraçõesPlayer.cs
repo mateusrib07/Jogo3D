47849d45a7ea13d6bfed7183e681504bf9804f27
Matheus Ribeiro <matheuzr14@gmail.com>
2024-12-03 12:33:39 -0300

    jogo final

--- Assets/Scripts/InteraçõesPlayer.cs
+++ Assets/Scripts/InteraçõesPlayer.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class InteraçõesPlayer : MonoBehaviour
 {
+    [SerializeField] int countKey = 0;
+    [SerializeField] int countCoin = 0;
+    [SerializeField] GameObject Coin;
+    [SerializeField] GameObject Key;
+    TextMeshPro chave;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,8 +21,32 @@ public class InteraçõesPlayer : MonoBehaviour
     {
         
     }
-    public void OnColision(Collider other)
+    public void OnCollisionEnter(Collision other)
     {
-        
+        if(other.gameObject.CompareTag("House"))
+        {
+            SceneManager.LoadScene("House");
+        }
+
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            countCoin++;
+        }
+
+        if(other.gameObject.CompareTag("Door"))
+        {
+            SceneManager.LoadScene("Fase1");
+        }
+
+        if(other.gameObject.CompareTag("key"))
+        {
+            Destroy(other.gameObject);
+            countKey++;
+        }
+        if (other.gameObject.CompareTag("Dungeon") && countKey == 1)
+        {
+            SceneManager.LoadScene("castle");
+        }
     }
 }
